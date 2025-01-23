9e0cb807552556fcc8b060cfb1c5debde58309a1
mdoradom <mariodoradomartinez@gmail.com>
2025-01-23 23:16:31 +0100

    audio

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,10 +1,18 @@
 extends Control
 
+func play_button_sound() -> void:
+	var sounds = ["button_click_1", "button_click_2", "button_click_3", "button_click_4"]
+	var random_sound = sounds[randi() % sounds.size()]
+	AudioManager.play_sound_3d(random_sound, Vector3.ZERO)
+
 func _on_start_button_pressed() -> void:
+	play_button_sound()
 	get_tree().change_scene_to_file("res://scenes/levels/playground.tscn")
 
 func _on_options_button_pressed() -> void:
+	play_button_sound()
 	get_tree().change_scene_to_file("res://scenes/ui/options.tscn")
 
 func _on_quit_button_pressed() -> void:
+	play_button_sound()
 	get_tree().quit()
