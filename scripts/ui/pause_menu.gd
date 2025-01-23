59f22ad03ef490bf7f8212de347ca2547df0b656
mdoradom <mariodoradomartinez@gmail.com>
2025-01-23 19:26:53 +0100

    Update pause_menu.gd

--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -5,7 +5,6 @@ var is_paused: bool = false:
 		is_paused = value
 		get_tree().paused = is_paused
 		visible = is_paused
-		# Toggle mouse capture and visibility
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if is_paused else Input.MOUSE_MODE_CAPTURED
 		
 func _ready():
@@ -20,9 +19,11 @@ func _on_resume_button_pressed():
 	is_paused = false
 
 func _on_options_button_pressed():
+	is_paused = false
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	get_tree().change_scene_to_file("res://scenes/ui/options.tscn")
 
 func _on_quit_button_pressed():
 	is_paused = false
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE  # Ensure mouse is visible in main menu
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
