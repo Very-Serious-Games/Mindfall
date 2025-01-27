08f31c681717c051164cd4d815ed6aee42811a5e
mdoradom <mariodoradomartinez@gmail.com>
2025-01-27 17:30:31 +0100

    credit scene NO TEXT

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -12,6 +12,10 @@ func _on_start_button_pressed() -> void:
 func _on_options_button_pressed() -> void:
 	play_button_sound()
 	get_tree().change_scene_to_file("res://scenes/ui/options.tscn")
+	
+func _on_credits_button_pressed() -> void:
+	play_button_sound()
+	get_tree().change_scene_to_file("res://scenes/ui/credits.tscn")
 
 func _on_quit_button_pressed() -> void:
 	play_button_sound()
