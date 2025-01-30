7e10d8930e7ccf6a3bf3f9dcc66297496546a17c
mdoradom <mariodoradomartinez@gmail.com>
2025-01-30 09:30:17 +0100

    build completa nen

--- scripts/text_good_ending.gd
+++ scripts/text_good_ending.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _on_button_pressed() -> void:
 	AudioManager.play_ui_sound("menu_select")
 	get_tree().change_scene_to_file("res://scenes/levels/good_engind.tscn")
