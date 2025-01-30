d9a3f1ee068d2bbe8d54a1a08ae94b1d6c690270
Nicolás Sort Sauquillo <nicolassortsauquillo@gmail.com>
2025-01-30 12:23:48 +0100

    musiquitaapplvl3

--- scripts/level_3.gd
+++ scripts/level_3.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var level_music = preload("res://assets/audio/music/Deprived Of Warmth.ogg")
+@onready var level_music = preload("res://assets/audio/music/Sxnctuary.ogg")
 
 func _ready() -> void:
 	AudioManager.play_music(level_music)
